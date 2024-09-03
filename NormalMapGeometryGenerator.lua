26058f939aaa7a1326c76db078d4ba32586a40da
Oscar Yang <oscaryangb@gmail.com>
2024-09-03 13:25:53 -0400

    Add rounding option

--- NormalMapGeometryGenerator.lua
+++ NormalMapGeometryGenerator.lua
@@ -1,7 +1,15 @@
 local dialog = Dialog("Normal Map Shape Generator")
 
 function coordToColor(coord)
-  return (coord / 2 + 0.5) * 255
+  return roundTo((coord / 2 + 0.5) * 255, tonumber(dialog.data.roundedTo))
+end
+
+function roundTo(number, to)
+  if to == 0 then
+    return number
+  end
+  
+  return math.floor(number - number % to)
 end
 
 function getLayer(layerName)
@@ -17,11 +25,10 @@ function getLayer(layerName)
 end
 
 function drawNormal()
+  local radius = tonumber(dialog.data.radius)
   local layer = getLayer(dialog.data.outputLayer)
   
-  local outputImage = Image(200, 300)
-  
-  local radius = tonumber(dialog.data.radius)
+  local outputImage = Image(radius * 2, radius * 2)
   
   for x = -radius, radius do
     for y = -radius, radius do
@@ -37,17 +44,16 @@ function drawNormal()
         r = coordToColor(x / magnitude), 
         g = coordToColor(-y / magnitude), 
         b = coordToColor(z / magnitude), 
-        a = 255
       }
 
-      --print(coordToColor(x / magnitude))
       outputImage:drawPixel(x + radius, y + radius, color)
       
       ::continue::
     end
   end
   
-  app.activeSprite:newCel(layer, 1, outputImage, Point(tonumber(dialog.data.xPosition), tonumber(dialog.data.yPosition)))
+  app.activeSprite:newCel(layer, 1, outputImage, 
+    Point(tonumber(dialog.data.xPosition) - radius, tonumber(dialog.data.yPosition) - radius))
   app.refresh()
 end
 
@@ -73,6 +79,12 @@ dialog:entry {
   id="radius",
   label = "Radius: ",
   text = "10"
+}
+
+dialog:entry {
+  id="roundedTo",
+  label = "Rounded To: ",
+  text = "1"
 }
 
 dialog:button {
